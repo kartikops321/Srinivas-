a1c494aced75d6201152f61cf8533b83db71cff6
kartikops321 <166072903+kartikops321@users.noreply.github.com>
2026-04-07 13:46:02 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,8 @@
+export const metadata = {
+  title: "Srinivasa Enterprises",
+  description: "Premium import-export website for agricultural products",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
